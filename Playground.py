24e2d9d2601f9a944fdc0b8020fa5f14ddf0ce55
Electrifying-arch <57690381+Electrifying-arch@users.noreply.github.com>
2020-10-02 08:23:36 -0700

    Create Playground.py

---
+++ Playground.py
@@ -0,0 +1,34 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:37:30) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hi Python!")
+Hi Python!
+>>> print("I'm so happy to be learning how to code in Python!")
+I'm so happy to be learning how to code in Python!
+>>> print(I'm so happy to be learning how to code in Python!')
+SyntaxError: invalid syntax
+>>> print('I\'m so happy to be learning howto code in Python!')
+I'm so happy to be learning howto code in Python!
+>>> "(I'm so happy to be learning how to code in Python!")
+SyntaxError: unmatched ')'
+>>> 
+>>> print("I'm so happy to be learning how to code in Python!")
+I'm so happy to be learning how to code in Python!
+>>> print("\Kumusta\" is \"Hello\" in Tagalog!")
+\Kumusta" is "Hello" in Tagalog!
+>>> print("\"Kumusta\" is \"Hello\" in Tagalog!")
+"Kumusta" is "Hello" in Tagalog!
+>>>  print("Here is \na sentenance \non many \ndifferent lines.")
+ 
+SyntaxError: unexpected indent
+>>> print("Here is \na sentence \non many \ndifferent lines.")
+Here is 
+a sentence 
+on many 
+different lines.
+>>> author = "Adrienne"
+>>> reader = "Sandi"
+>>> print (reader)
+Sandi
+>>> print (author)
+Adrienne
+>>> 
